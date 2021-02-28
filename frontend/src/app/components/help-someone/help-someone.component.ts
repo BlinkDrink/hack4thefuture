d967cc2ba4d55770009bcebe0d06bd39a4a9e468
Dimitar <dimitar.trz@gmail.com>
2021-02-28 04:29:39 +0200

    User help
    Adding user help models and basic cards

--- frontend/src/app/components/help-someone/help-someone.component.ts
+++ frontend/src/app/components/help-someone/help-someone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HelpUser } from 'src/app/models/helpuser';
 
 @Component({
   selector: 'app-help-someone',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpSomeoneComponent implements OnInit {
 
+  helpUsers: HelpUser[] = [
+    {
+      user: {
+        name: 'Petar'
+      },
+      material: {
+        id: '3a94f370-e5fe-4fb0-ad1d-598bd5308214',
+        name: 'Arrays',
+        link: 'https://en.wikipedia.org/wiki/Array_data_structure',
+        questions: [],
+        score: 0
+      }
+    },
+    {
+      user: {
+        name: 'Georgi'
+      },
+      material: {
+        id: '1b2c214f-e255-4e05-bfdb-f0026be3cc69',
+        name: 'Dynamic memory',
+        link: 'https://www.cplusplus.com/doc/tutorial/dynamic/',
+        questions: [],
+        score: 20
+      }
+    },
+    {
+      user: {
+        name: 'Petkan'
+      },
+      material: {
+        id: '95f0eb44-d1bc-4058-a9f2-35cfa1505611',
+        name: 'Functions',
+        link: 'https://www.cplusplus.com/doc/tutorial/functions/',
+        questions: [],
+        score: 30
+      }
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
---
+++ frontend/src/app/models/helpuser.ts
@@ -0,0 +1,7 @@
+import { Material } from './material';
+import { User } from './user';
+
+export class HelpUser {
+    user!: User;
+    material!: Material;
+}
